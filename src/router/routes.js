5dc1a594d818794a9549504afa6ee2f827f63d99
infun-soso <973394690@qq.com>
2018-11-22 14:38:32 +0800

    add lazyload router

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,19 +2,56 @@ import React from 'react'
 import {
 	Route,
 } from 'react-router-dom';
-import Dashboard from '../pages/dashboard'
-import AddArticle from '../pages/article/addArticle'
-import ArticleList from '../pages/article/articleList'
-import ToDoList from '../pages/tools/todolist'
+import Loadable from 'react-loadable';
+
+const RouteWithSubRoutes = route => (
+	<Route
+		path={route.path}
+		render={props => (
+			<route.component {...props} routes={route.routes}	/>
+		)}
+	></Route>
+)
+
+const Loading = (props) => {
+	return <div>Loading...</div>
+}
+
+const LazyLoad = loader => Loadable({
+	loader,
+	loading: Loading
+})
+
+const routeMap = [
+	{
+		path: '/dashboard',
+		name: '首页',
+		component: LazyLoad(() => import('../pages/dashboard'))
+	},
+	{
+		path: '/addarticle',
+		name: '添加文章',
+		component: LazyLoad(() => import('../pages/article/addArticle'))
+	},
+	{
+		path: '/articlelist',
+		name: '文章列表',
+		component: LazyLoad(() => import('../pages/article/articleList'))
+	},
+	{
+		path: '/todolist',
+		name: '小工具',
+		component: LazyLoad(() => import('../pages/tools/todolist'))
+	},
+]
 
 export default class Section extends React.Component {
 	render() {
 		return (
 			<div style={{ padding: 24, background: '#fff', minHeight: 360, position: "relative" }}>
-				<Route exact path="/dashboard" component={Dashboard}/>
-				<Route exact path="/addarticle" component={AddArticle}/>
-				<Route exact path="/articlelist" component={ArticleList}/>
-				<Route exact path="/todolist" component={ToDoList}/>
+				{
+					routeMap.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)
+				}
 			</div>	
 		)
 	}
